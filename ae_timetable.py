52570521e764a8ac452422109deec15b8583b89b
Michael Brunton-Spall <michael.brunton-spall@guardian.co.uk>
2010-07-19 15:55:18 +0100

    Update cron to run once an hour

--- ae_timetable.py
+++ ae_timetable.py
@@ -24,25 +24,68 @@ def get_events_list(icsfile):
     return icalendar.Calendar.from_string(icsfile).walk('vevent')
     
 def get_speaker_and_title(event):
-    t = event.split(':')
+    t = event['summary'].split(':')
     if len(t) > 1:
-        return t
-    else
+        return t[0],":".join(t[1:])
+    else:
         return None, t[0]
+
+def get_duration(event):
+    try: 
+        return event['duration'].dt.seconds / 60
+    except KeyError:
+        return 0
+
+def get_location(event):
+    try: 
+        return event['location']
+    except KeyError:
+        return "Unknown"
+    
+def get_id(event):
+    return event['uid'].split('@')[0]
+    
+def get_start_time(event):
+    return event['dtstart'].dt
     
 def persist_events(events):
+    db.delete(Event.all())
     for event in events:
         speaker, title = get_speaker_and_title(event)
         e = Event()
-        
+        e.id = get_id(event)
+        e.speaker, e.title = get_speaker_and_title(event)
+        e.duration = get_duration(event)
+        e.location = str(get_location(event))
+        e.start_time = get_start_time(event)
+        e.put()
 
 class FetchIcs(webapp.RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/plain'
 		content = get_events_list(get_local_ics_file())
-		self.response.out.write([c for c in content])
+		persist_events(content)
+        # for c in content:         
+        #     self.response.out.write(get_start_time(c))
+        #     self.response.out.write("\t")
+        #     self.response.out.write(get_id(c))
+        #     self.response.out.write("\t")
+        #     self.response.out.write(get_location(c))
+        #     self.response.out.write("\t")
+        #     self.response.out.write(get_duration(c))
+        #     self.response.out.write("\t")
+        #     self.response.out.write(get_speaker_and_title(c))
+        #     self.response.out.write("\n")
+        
+class AllEvents(webapp.RequestHandler):
+    def get(self):
+        for e in Event.all().order('start_time'):
+            self.response.out.write('<li>%s - %s</li>' % (e.start_time, e.title))
 
-application = webapp.WSGIApplication([('/services/fetch_ics', FetchIcs)],
+application = webapp.WSGIApplication([
+            ('/services/fetch_ics', FetchIcs),
+            ('/', AllEvents),
+                                        ],
                                      debug=True)
 
 def main():
